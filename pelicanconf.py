5e10f9d45e5f1a787aa4d408de1d10f5ea9fc6c6
40823251 <40823251@gm.nfu.edu.tw>
2021-02-28 00:37:19 +0800

    add new blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'cmstemplate user'
-SITENAME = 'cmstemplate blog'
+AUTHOR = '40823251'
+SITENAME = 'cd2021 40823251'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
